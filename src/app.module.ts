0bae3b1ab3ff0ef00a6cda6506696c579e809f62
Saul Burgos <saulburgos6@gmail.com>
2023-07-26 14:00:25 -0400

    add mod auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { CategoryModule } from './category/category.module';
 import { DatabaseModule } from './database/database.module';
 import { OrderModule } from './order/order.module';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ProductModule, CategoryModule, DatabaseModule, OrderModule, UserModule
+  imports: [ProductModule, CategoryModule, DatabaseModule, OrderModule, UserModule, AuthModule
 
 
   ],
