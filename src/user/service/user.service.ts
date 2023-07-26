adb1ffd00c79c99bdb1009472b6affc9869842f9
Saul Burgos <saulburgos6@gmail.com>
2023-07-26 12:41:58 -0400

    add user services

--- src/user/service/user.service.ts
+++ src/user/service/user.service.ts
@@ -1,4 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from '../domain/user.model';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+
+
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>
+  ) { }
+
+  public async findOne(id: string): Promise<User> {
+    const user = await this.userRepository.findOne({
+      where: { id }
+
+    })
+    if (!user) {
+      throw new NotFoundException(`User #${id} not found`);
+    }
+
+    return user
+
+  }
+
+
+  public async create(user: User): Promise<void> {
+    await this.userRepository.save(user)
+  }
+}
